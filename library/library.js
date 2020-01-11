9213ec714f3094bb8546d8e3c0b47e9ac03b573c
XENIA <lommy101@gmail.com>
2020-01-12 03:07:32 +0900

    Update library.js

--- library/library.js
+++ library/library.js
@@ -1,6 +1,7 @@
 var client = require('cheerio-httpcli');
 
 let url = 'https://library.hoseo.ac.kr/smufu-api/pc/2/rooms-at-seat?branchGroupId=1&isActive=true';
+//let url = 'https://library.hoseo.ac.kr/smufu-api/pc/2/rooms-at-seat?branchGroupId=2&isActive=true';
 
 
 client.set('headers', { 
@@ -16,6 +17,14 @@ client.fetch(url, {}, function(err, $, res){
     else{
         data = JSON.parse($.html());
         data = data['data']['list'];
-
+        
+        for (let index = 0; index < data.length; index++) {
+            let name = data[index]['name'];
+            let total = data[index]['total'];
+            let occupied = data[index]['occupied'];
+            let available = data[index]['available'];
+            let percent = Math.round(occupied/total * 100);
+            console.log(name + '\n전체 좌석수 : ' + total + '\n사용 좌석수 : ' + occupied  + '\n잔여 좌석수 : ' + available + '\n이용율 : ' + percent + '%\n');
+        }
     }
 });
